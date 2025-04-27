b20edf2588ce16b51de6767f50118ebd09374b35
floatpigeon <floatpigeon@proton.me>
2025-04-27 18:25:10 +0800

    Creat basic resources

---
+++ src/dartlauncher_guidance.cpp
@@ -0,0 +1,25 @@
+#include <rclcpp/logger.hpp>
+#include <rclcpp/logging.hpp>
+#include <rclcpp/node.hpp>
+#include <rmcs_executor/component.hpp>
+
+namespace rmcs_dart_guide {
+
+class DartLauncherGuidance
+    : public rmcs_executor::Component
+    , public rclcpp::Node {
+public:
+    DartLauncherGuidance()
+        : Node(get_component_name(), rclcpp::NodeOptions{}.automatically_declare_parameters_from_overrides(true))
+        , logger_(get_logger()) {}
+
+    void update() override { RCLCPP_INFO(logger_, "test"); }
+
+private:
+    rclcpp::Logger logger_;
+};
+} // namespace rmcs_dart_guide
+
+#include <pluginlib/class_list_macros.hpp>
+
+PLUGINLIB_EXPORT_CLASS(rmcs_dart_guide::DartLauncherGuidance, rmcs_executor::Component)
